e107117f0d6d2e60bdabcf921153fc78c88cbb28
idho <user@DESKTOP-EVMAOA5>
2026-09-09 20:27:28 +0700

    akun sudah bisa login

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,6 +22,7 @@ class DatabaseSeeder extends Seeder
             'email' => 'test@example.com',
         ]);
 
+        $this->call(UserSeeder::class);
         $this->call(ProductSeeder::class);
     }
 }

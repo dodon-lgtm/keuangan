3a3edca872f8f42c4177c502c2a4f0cb936a801e
Ari Sigit Firdaus <email-github-anda@gmail.com>
2026-09-18 10:45:33 +0700

    jaubah jadi hijab

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -5,7 +5,7 @@
 @section('content')
     <div class="d-flex justify-content-between align-items-center mb-3">
         <h1>Produk</h1>
-        <a href="{{ route('products.create') }}" class="btn btn-primary">+ Produk Jaubah</a>
+        <a href="{{ route('products.create') }}" class="btn btn-primary">+ Produk Hijab</a>
     </div>
 
     <table class="table table-striped align-middle">
